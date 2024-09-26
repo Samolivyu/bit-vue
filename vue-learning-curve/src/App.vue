8d590ba9befcc1b4b81cd337a73636c666b7d02c
Nesh <95473914+Samolivyu@users.noreply.github.com>
2024-09-26 06:07:15 +0300

    Boiler plate setup.

---
+++ vue-learning-curve/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+
+      <h1> VUE LEARNING CURVE </h1>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
